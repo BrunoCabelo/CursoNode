2efd221bec94390b2a9701b7446513b2b1e451a6
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-03-12 13:31:55 -0300

    inicio do projeto 2
    Iniciando bibliotecas, criando conexão com database...

---
+++ projeto2/database/connection.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+
+const connection = new Sequelize('guiapress', 'root', '123456',{
+    port: 3336,
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
