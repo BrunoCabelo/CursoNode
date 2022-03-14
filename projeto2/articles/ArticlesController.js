67d1bf70b8581ef5b6e80a6c2b3066bb50cb4de7
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-03-14 20:33:10 -0300

    Automação de artigos e categorias
    Foram criadas as funções de edição, criação, exclusão e atualização de artigos e categorias

--- projeto2/articles/ArticlesController.js
+++ projeto2/articles/ArticlesController.js
@@ -1,12 +1,100 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const Slugify = require('slugify');
+const { default: slugify } = require('slugify');
 
-router.get('/articles', (req, res)=>{
-    res.send("Rota de artigos");
+router.get('/admin/articles', (req, res)=>{
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles =>{
+        res.render('admin/articles/index',{
+            articles : articles
+        })
+    })
+    
 });
 
 router.get('/admin/articles/new', (req, res)=>{
-    res.send("Rota de um novo artigo");
+    Category.findAll().then(categories=>{
+        res.render('admin/articles/new',
+            {categories: categories}
+        );
+    })
+    
 });
+
+router.post('/articles/save', (req,res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+    if(title != undefined && body != undefined && category != undefined ){
+        Article.create({
+            title: title,
+            slug: Slugify(title),
+            body: body,
+            categoryId: category
+        }).then(()=>{
+            res.redirect('/admin/articles')
+        }).catch((erro)=>{
+            console.log(erro);
+            res.redirect('/admin/articles/new');
+        })
+    }else{
+        res.redirect('/admin/articles/new');
+    }
+});
+
+router.post('/articles/delete', (req,res)=>{
+    var id = req.body.id;
+    Article.destroy({
+        where:{
+            id: id
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles');
+    })
+});
+
+router.get('/admin/articles/edit/:id', (req,res)=>{
+    var id = req.params.id;
+    if(id != undefined){
+        Article.findByPk(id, {include: [{model: Category}]}).then((article)=>{
+            if(article != undefined){
+                Category.findAll().then(categories=>{
+                    res.render('admin/articles/edit',{article: article, categories: categories});
+                })
+                
+            }else{
+                res.redirect('/admin/articles');
+            }
+            
+        });
+    }else{
+        res.redirect('/admin/articles');
+    };
+    
+});
+
+router.post('/articles/update', (req,res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var categoryId = req.body.category;
+    var id = req.body.id;
+   
+    Article.update({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoryId
+    },{
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles');
+    })
+})
 
 module.exports = router;
--- projeto2/database/connection.js
+++ projeto2/database/connection.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress', 'root', '123456',{
     port: 3336,
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
