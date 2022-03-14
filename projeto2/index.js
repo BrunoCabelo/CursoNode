76845965f936d4b135969afc123052f346ed0426
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-03-14 09:38:57 -0300

    Definindo arquitetura do sistema

--- projeto2/index.js
+++ projeto2/index.js
@@ -2,16 +2,30 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/connection');
+
+const catergoriesController = require('./categories/CatergoriesController');
+const articlesController = require('./articles/ArticlesController');
+
 //View engine
 app.set('view engine', 'ejs');
 
 //static
 app.use(express.static('public'));
 
+// Database
+connection.authenticate().then(()=>{
+    console.log("Conexão feita com sucesso!");
+}).catch((erro)=>{
+    console.log(erro);
+})
 
 //body-parser
-app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+
+app.use('/', catergoriesController);
+app.use('/', articlesController);
 
 
 app.get('/', (req, res)=>{
