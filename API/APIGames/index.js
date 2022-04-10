73a8d92b681f73afcc29912deb7aed281074d1cb
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-04-10 10:15:30 -0300

    update

--- API/APIGames/index.js
+++ API/APIGames/index.js
@@ -2,11 +2,41 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+
+const jwtsecreet = 'as12d24pt0-123';
+
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+
+function auth(req, res, next){
+ 
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ');
+        const token = bearer[1];
+        
+        jwt.verify(token, jwtsecreet,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err: 'Token invalido'});
+            }else{
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email};
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err: 'Token invalido'});
+    }
+}
 
 var DB = {
     games : [
@@ -40,11 +70,24 @@ var DB = {
             "year": "1999",
             "price": 25
         }
+    ],
+    users: [
+        {
+            id: 1,
+            name: 'José',
+            email: 'jose@jose',
+            password: '1234'
+        },{
+            id: 2,
+            name: 'Victor',
+            email: 'victor@victor',
+            password: 'java123'
+        }
     ]
 }
 
 
-app.get('/games', (req, res) => {
+app.get('/games', auth ,(req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
 });
@@ -69,8 +112,8 @@ app.get('/game/:id', (req,res) => {
     }
 });
 
-app.post('/game', (req,res) => {
-
+app.post('/game', auth , (req,res) => {
+    
     var title = req.body.title;
     var year = req.body.year;
     var price = req.body.price;
@@ -89,7 +132,7 @@ app.post('/game', (req,res) => {
 
 });
 
-app.delete('/game/:id', (req,res) => {
+app.delete('/game/:id', auth , (req,res) => {
     var id = req.params.id;
     if(isNaN(id)){
         res.sendStatus(400)
@@ -105,7 +148,7 @@ app.delete('/game/:id', (req,res) => {
     }
 });
 
-app.put('/game/:id', (req,res) => {
+app.put('/game/:id', auth , (req,res) => {
     var id = req.params.id;
 
     if(isNaN(id)){
@@ -136,6 +179,39 @@ app.put('/game/:id', (req,res) => {
 
     }
 });
+
+app.post('/auth', (req, res) =>{
+    var {email, password} = req.body;
+    if(email != undefined){
+
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            
+            if(user.password == password){
+                var token = jwt.sign({id: user.id, email: user.email, name: user.name}, jwtsecreet,{expiresIn: '24h'}, (err, token) => {
+                    if(err){
+                        res.status(400),
+                        res.json({err: 'falha interna'});
+                    }else{
+                        res.status(200);
+                        res.json({token: token})
+                    }
+                });
+            }else{
+                res.status(401);
+                res.json({err: 'Credenciais invalidas'});
+            }
+
+        }else{
+            res.status(404);
+            res.json({err: 'O email enviado não existe'});
+        }
+
+    }else{
+        res.status(400);
+        res.json({err: 'O email é invalido'});
+    }
+})
 
 app.listen(8080,() =>{
     console.log('server api iniciado');
